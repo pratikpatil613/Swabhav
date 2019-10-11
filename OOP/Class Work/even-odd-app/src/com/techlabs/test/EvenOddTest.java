84d162accbebfc54cc58978aaa1895af10916741
kp397 <kpprp123@gmail.com>
2019-10-11 20:18:48 +0530

    oop

---
+++ OOP/Class Work/even-odd-app/src/com/techlabs/test/EvenOddTest.java
@@ -0,0 +1,17 @@
+package com.techlabs.test;
+
+public class EvenOddTest {
+	public static void main(String args[])
+	{
+		int a =10;
+		if(a%2==0)
+		{
+			System.out.println("EVEN NO");
+		}
+		else
+		{
+			System.out.println("odd no");
+		}
+	}
+
+}
